f75830a4df2d831e0fed9de2dd0e1c7bda2af834
jeonghyeyeon1 <https://github.com/jeonghyeyeon1>
2023-11-26 01:17:18 +0900

    상세페이지 생성 및 링크연결

--- community/views.py
+++ community/views.py
@@ -4,10 +4,22 @@ from .models import Post
 # Create your views here.
 def index(request):
     posts = Post.objects.all().order_by('-pk') # 모든 post 레코드 가져와 posts에 저장
+
     return render(
         request,
         'community/index.html',
         {
             'posts': posts,  # 딕셔너리 형태로 추가
+        }
+    )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk) # 조건 만족하는 post 레코드 가져옴
+
+    return render(
+        request,
+        'community/single_post_page.html',
+        {
+            'post':post,
         }
     )
