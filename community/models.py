e11c35f451ada4e27b1cc895737be2ae08ec5058
jeonghyeyeon1 <https://github.com/jeonghyeyeon1>
2023-11-26 00:52:27 +0900

    Post 모델 생성

--- community/models.py
+++ community/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+    # <Post객체>.publish 입력시, 해당 객체의 published_date에 현재시간을 넣어주는 함수
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
