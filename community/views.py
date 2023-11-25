11234b1d8074a84fbd8f801c37f154d05ea918ff
jeonghyeyeon1 <https://github.com/jeonghyeyeon1>
2023-11-26 01:40:35 +0900

    CBV로 포스트 목록 생성

--- community/views.py
+++ community/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from .models import Post
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk') # 모든 post 레코드 가져와 posts에 저장
 
-    return render(
-        request,
-        'community/index.html',
-        {
-            'posts': posts,  # 딕셔너리 형태로 추가
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk) # 조건 만족하는 post 레코드 가져옴
