cb8d77a2e1ea25d7e0d464ec81edcffe4bede831
jeonghyeyeon1 <https://github.com/jeonghyeyeon1>
2023-11-26 01:05:49 +0900

    포스트 목록 페이지 생성

--- community/views.py
+++ community/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk') # 모든 post 레코드 가져와 posts에 저장
+    return render(
+        request,
+        'community/index.html',
+        {
+            'posts': posts,  # 딕셔너리 형태로 추가
+        }
+    )
